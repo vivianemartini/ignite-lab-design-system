4dc7c0ea625ba0ce3e2a9cb7a77e9cf68b34fda1
Vivi - pessoal <martini.viviane@gmail.com>
2022-10-16 12:03:14 -0300

    fix ci

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -15,9 +15,9 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
-  viteFinal: (config, {configType}) => {
+  viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = 'ignite-lab-design-system'
+      config.base = '/ignite-lab-design-system'
     }
 
     return config
